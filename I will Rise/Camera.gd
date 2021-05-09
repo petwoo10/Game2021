fa4a0ce0fbda87ba0223a12cd04917bc83fad6d2
petwoo10 <82476384+petwoo10@users.noreply.github.com>
2021-05-10 11:28:47 +1200

    chaerter moving

---
+++ I will Rise/Camera.gd
@@ -0,0 +1,16 @@
+extends Camera
+
+
+onready var player_follow = get_parent().get_node("Player")
+onready var player_outline = $RayCast
+
+
+
+func _process(delta):
+	var camera_pos = Vector3.ZERO
+	camera_pos.x = player_follow.translation.x-15
+	camera_pos.z = player_follow.translation.z-15
+	camera_pos.y = player_follow.translation.y+15
+	set_translation(camera_pos)
+	if player_outline.get_collider():
+		print(player_outline)
---
+++ I will Rise/Player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody
+
+
+onready var camera = get_parent().get_node("Camera")
+
+export var max_speed = 10
+export var friction = 10 
+export var speed = 1
+
+var move_vector = Vector3.ZERO
+var cursor_pos = Vector3.ZERO
+
+
+func look_at_cursor():
+	var player_pos = global_transform.origin
+	var drop_plane = Plane(Vector3(0,1,0), player_pos.y)
+	var ray_length = 1000
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) *ray_length
+	cursor_pos = drop_plane.intersects_ray(from,to)
+	look_at(cursor_pos,Vector3.UP)
+	
+	
+func get_input():
+	var input = Vector3(
+		-int(Input.is_action_pressed("ui_right")) + int(Input.is_action_pressed("ui_left")),
+		0,
+		-int(Input.is_action_pressed("ui_down")) + int(Input.is_action_pressed("ui_up"))
+	)
+	input = input.normalized()
+	return input
+	
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	var input = get_input()
+	if input != Vector3.ZERO:
+		speed += .1
+		if speed > max_speed:
+			speed = max_speed
+			
+	move_vector = input * speed
+	move_and_slide(move_vector)
+	
+	look_at_cursor()
+
+	
+
+
+
+
+
