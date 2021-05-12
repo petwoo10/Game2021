5b73d13e77ef88f53c0b44a72a25c6c3004b251e
petwoo10 <82476384+petwoo10@users.noreply.github.com>
2021-05-12 14:00:09 +1200

    graxty

--- I will Rise/Player.gd
+++ I will Rise/Player.gd
@@ -2,14 +2,18 @@ extends KinematicBody
 
 
 onready var camera = get_parent().get_node("Camera")
+onready var groundcheck = $groundcheck
 
 export var max_speed = 10
 export var friction = 10 
 export var speed = 1
 
+var direction = Vector3()
 var move_vector = Vector3.ZERO
 var cursor_pos = Vector3.ZERO
-
+var gravity = 9.8
+var gravity_vec = Vector3()
+var velocity = Vector3.ZERO
 
 func look_at_cursor():
 	var player_pos = global_transform.origin
@@ -35,14 +39,28 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
+	direction = Vector3()
+	
+	#velocity = move_and_slide(velocity, Vector3.UP)
+
+	
 	var input = get_input()
 	if input != Vector3.ZERO:
 		speed += .1
 		if speed > max_speed:
 			speed = max_speed
-			
+	if not is_on_floor():
+		input.y = -gravity 
+		print("flying")	
+	else:
+		input.y = 0
+		
+	print(input, move_vector)
+	
 	move_vector = input * speed
-	move_and_slide(move_vector)
+	move_vector = move_and_slide_with_snap(move_vector, Vector3.DOWN, Vector3.UP)
+	#print(move_vector)
+	
 	
 	look_at_cursor()
 
