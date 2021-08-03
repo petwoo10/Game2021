5d36f58b3f9c3c1596e1bae1a48b2c12794ddfed
petwoo10 <82476384+petwoo10@users.noreply.github.com>
2021-08-03 14:01:03 +1200

    cars

--- I will Rise/Player.gd
+++ I will Rise/Player.gd
@@ -15,9 +15,8 @@ var cursor_pos = Vector3.ZERO
 #var gravity = 9.8
 var gravity_vec = Vector3()
 var velocity = Vector3.ZERO
-var melee_damage= 50
+
 #player facing cursor
-onready var attck = $newplayer/AnimationPlayer
 
 export var gravity = Vector3.DOWN * 10
 export var speed = 8
@@ -25,15 +24,7 @@ export var rot_speed = 0.85
 
 #var velocity = Vector3.ZERO
 
-func look_at_cursor():
-	var player_pos = global_transform.origin
-	var drop_plane = Plane(Vector3(0,1,0), player_pos.y)
-	var ray_length = 1000
-	var mouse_pos = get_viewport().get_mouse_position()
-	var from = camera.project_ray_origin(mouse_pos)
-	var to = from + camera.project_ray_normal(mouse_pos) *ray_length
-	cursor_pos = drop_plane.intersects_ray(from,to)
-	look_at(cursor_pos,Vector3.UP)
+
 	
 	#inputs for moving
 
@@ -48,9 +39,9 @@ func get_input(delta):
 	if Input.is_action_pressed("back"):
 		velocity += transform.basis.z * speed
 	if Input.is_action_pressed("right"):
-		velocity+=transform.basis.x * speed#rotate_y(-rot_speed * delta)
+		rotate_y(-rot_speed * delta)
 	if Input.is_action_pressed("left"):
-		velocity-=transform.basis.x * speed#rotate_y(rot_speed * delta)
+		rotate_y(rot_speed * delta)
 	velocity.y = vy
 	
 	
@@ -64,20 +55,7 @@ func get_input(delta):
 	
 #	return input
 	
-func melee():
-	if Input.is_action_just_pressed("ui_accept"):
-		$newplayer/AnimationPlayer.play("attck")
-		var checkenemy = $hitbox.get_overlapping_bodies()
-		attacking = true
-		print("attacking")
-		for enermy in checkenemy:
-			if enermy.is_in_group("enemy"):
-				enermy.enermy_health -= 10
-				if enermy.enermy_health <= 0:
-					print("dead")
-					enermy.queue_free()
-		yield($newplayer/AnimationPlayer,"animation_finished")
-		attacking = false
+
 
 		
 	
@@ -113,17 +91,9 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP,true)
 	#move_vector = input * speed
 	#move_vector = move_and_slide(move_vector, Vector3.UP, true)
-	if not attacking:
-		if move_vector !=Vector3.ZERO:
-			$newplayer/AnimationPlayer.play("running")
-			
-		else:
-			$newplayer/AnimationPlayer.play("idle")
-		#move_vector = move_and_slide_with_snap(move_vector, Vector3.DOWN, Vector3.UP, true)
-		#print(move_vector)
-		
-	melee()
-	look_at_cursor()
+	
+	
+
 
 	
 
--- I will Rise/enermy.gd
+++ I will Rise/enermy.gd
@@ -21,11 +21,11 @@ func _physics_process(delta):
 		var facing = -global_transform.basis.z
 		look_at(pos,Vector3.UP)
 		move_and_slide(facing * move_speed *delta,Vector3.UP)
-		if $RayCast.get_collider() !=null:
-			if $RayCast.get_collider().name == "player":
-				$enemy/AnimationPlayer.play("runing")
-		else:
-			$enemy/AnimationPlayer.play("idle")
+
+
+
+		
+
 	
 	
 	
@@ -36,7 +36,6 @@ func _on_Area_body_entered(body):
 		print("fond player")
 		follow_player = true
 		player = body 
-
 
 func _on_Area_body_exited(body):
 	if body.name == "Player":
