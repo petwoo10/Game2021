67065d1dd6f13c7dacf3011c5325e0c12e99c06c
petwoo10 <82476384+petwoo10@users.noreply.github.com>
2021-08-06 10:57:46 +1200

    car now moving

---
+++ I will Rise/PlayerCar.gd
@@ -0,0 +1,24 @@
+extends VehicleBody
+
+
+var acclen = 400
+var turn_speed = 3
+var streing_taget 
+var braking = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	streing_taget = Input.get_action_strength("left")-Input.get_action_strength("right")
+	
+	if Input.is_action_pressed("forward"):
+		engine_force = acclen
+	else:
+		engine_force = 0 
+	
+	if Input.is_action_pressed("back"):
+		brake = braking
+	else:
+		brake = 0 
+	
+	steering = move_toward(steering, streing_taget,turn_speed *delta)	 
