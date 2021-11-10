07be4d9a8e064337fd3b71f77a5735d805bef374
petwoo10 <82476384+petwoo10@users.noreply.github.com>
2021-11-10 14:49:31 +1300

    done

--- I will Rise/spawner.gd
+++ I will Rise/spawner.gd
@@ -8,6 +8,6 @@ func _on_Timer_timeout():
 	var boulder = Boulder.instance()
 	add_child(boulder)
 	boulder.global_transform = global_transform
-	boulder.apply_central_impulse(Vector3(0,0,-1)*2000)
+	boulder.apply_central_impulse(Vector3(0,0,-1)*1500)
 	timer.set_wait_time(rand_range(4,10))
 	print(timer.wait_time)
